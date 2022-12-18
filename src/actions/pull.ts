cc58e65a00d4f49298a8f3cab073a5c01ab535aa
Jiahome Wu <jiahome_wu@wishmobile.com>
2022-12-19 00:11:33 +0800

    Feature: pull command init

---
+++ src/actions/pull.ts
@@ -0,0 +1,3 @@
+export default function pull(state_key: string) {
+    console.log(state_key);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { Command } from "commander";
 import InitState from "./actions/InitState";
+import pull from "./actions/pull";
+import { DEFAULT_STATE_KEY } from "./types/state";
 
 const program = new Command();
 
 program
   .command('init')
   .action(InitState);
+
+program
+  .command('pull')
+  .option('-k, --key <stateKey>', 'state key for pull', DEFAULT_STATE_KEY)
+  .action((options)=>{
+    pull(options.key);
+  })
 
 program.parse(process.argv)
