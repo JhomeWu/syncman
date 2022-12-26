a8ef04cda5166d4c107cef54bbfbc3f34a75efb1
Jiahome Wu <jiahome_wu@wishmobile.com>
2022-12-26 17:32:12 +0800

    Feature: Pull

--- src/actions/pull.ts
+++ src/actions/pull.ts
@@ -1,3 +1,21 @@
-export default function pull(state_key: string) {
-    console.log(state_key);
+import { writeFileSync } from "fs";
+import { PulledResource } from "../types/resources";
+import PostmanRequestHelper from "../helpers/PostmanRequestHelper";
+import StateHelper from "../helpers/StateHelper";
+
+export default async function pull(stateKey: string) {
+    const currentState = StateHelper.get(stateKey);
+    const resource = await PostmanRequestHelper.get(`/${currentState.type}/${currentState.id}`);
+    writeFileSync(currentState.path, JSON.stringify(resource, null, 2));
+    currentState.pulled = (
+        currentState.pulled ||
+        [
+            {
+                id: currentState.id,
+                time: new Date().toISOString()
+            } as PulledResource
+        ]
+    );
+    StateHelper.set(currentState, stateKey);
+    StateHelper.store();
 }
